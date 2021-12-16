4b7d471ca44cfdd9eff21ab94df1796a84d78504
Matt Burnett <mburnett@tutanota.de>
2021-12-16 16:30:54 +0100

    tweaks to ProductDetail-specific code / tests

--- src/__tests__/components/ProductDetail.test.js
+++ src/__tests__/components/ProductDetail.test.js
@@ -1,28 +1,27 @@
 import { render, screen } from '@testing-library/react';
-import ProductDetail from '../../components/product/Product';
+import Product from '../../components/product/Product';
 
-describe('<ProductDetail /> basic render', () => {
-    // FIXME: this should be beforeAll, but beforeAll blows out the tests after first test is run...
+describe('<Product /> basic render', () => {
+    // FIXME: this should be beforeAll, but beforeAll blows out the tests after the first one runs...
     beforeEach(() => {
-        render(<ProductDetail />);
+        render(<Product />);
     });
 
     it('should have an image to display', () => {
         screen.getByRole('img', {name: /product-image/i});
     });
     it('should have a description', () => {
-        // screen.getByRole('presentation', {name: /description/i});
-        expect(screen.getByText(/description/i)).toBeInTheDocument();
+        screen.getByRole('presentation', {name: /description/i});
+        // expect(screen.getByText(/description/i)).toBeInTheDocument();
     });
     it('should show a price', () => {
-        // screen.getByRole('presentation', {name: /price/i});
-        expect(screen.getByText(/price/i)).toBeInTheDocument();
+        screen.getByRole('presentation', {name: /price/i});
+        // expect(screen.getByText(/price/i)).toBeInTheDocument();
     });
     it('should have a quantity selector', () => {
-        // screen.getByRole('select', {name: /quantity-selector/i});
-        expect(screen.getByText(/qty/i)).toBeInTheDocument();
+        screen.getByRole('textbox', {name: /quantity/i});
     });
-    it('should have an add-to-cart button', function () {
+    it('should have an add-to-cart button', () => {
         screen.getByRole('button', {name: /add-to-cart/i});
     });
 })
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,15 +1,13 @@
-function Product (props) {
+export default function Product (props) {
     const { img, name, description, price }  = props;
     return (
         <div>
             <h2>{name} Details</h2>
-            <img aria-label="product-image" src={img} alt="product"/>
+            <img aria-label="product-image" src={img} alt={name}/>
             <div role="presentation" aria-label="description">Description: {description}</div>
             <div role="presentation" aria-label="price">Price: {price}</div>
-            <div role="select" aria-label="quantity-selector">Qty</div>
+            <input aria-label='quantity' placeholder="Enter product quantity" />
             <button aria-label="add-to-cart">Add To Cart</button>
         </div>
     )
 }
-
-export default Product;
