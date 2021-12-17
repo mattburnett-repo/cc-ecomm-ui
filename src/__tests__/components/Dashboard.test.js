b6f7c6f6565cb8662d0d0a097fd9db969522016e
Matt Burnett <mburnett@tutanota.de>
2021-12-17 19:14:05 +0100

    Dashboard code / tests. Not complete yet

--- src/__tests__/components/Dashboard.test.js
+++ src/__tests__/components/Dashboard.test.js
@@ -18,85 +18,7 @@ const mockUserData = {
     "google_image": null
 };
 
-const mockOrderData = {
-    "order": {
-        "cart": {
-            "cart_id": 1,
-            "cart_items": [
-                {
-                    "product_id": 1,
-                    "product_name": "product_name_01",
-                    "product_price": 1,
-                    "product_quantity": 1,
-                    "product_total_price": 1
-                },
-                {
-                    "product_id": 2,
-                    "product_name": "product_name_02",
-                    "product_price": 2,
-                    "product_quantity": 2,
-                    "product_total_price": 4
-                },
-                {
-                    "product_id": 3,
-                    "product_name": "product_name_03",
-                    "product_price": 3,
-                    "product_quantity": 3,
-                    "product_total_price": 9
-                },
-                {
-                    "product_id": 4,
-                    "product_name": "product_name_04",
-                    "product_price": 4,
-                    "product_quantity": 4,
-                    "product_total_price": 16
-                }
-            ]
-        },
-        "user_id": 1,
-        "order_id": 1,
-        "order_date": null,
-        "total_price": {
-            "sum": 30,
-            "cart_id": 1
-        }
-    }
-};
 
-const mockCartData = [
-    {
-        "id": 1,
-        "product_id": 1,
-        "name": "product_name_01",
-        "product_quantity": 1,
-        "product_price": "1.00",
-        "line_item_total_price": "1.00"
-    },
-    {
-        "id": 1,
-        "product_id": 2,
-        "name": "product_name_02",
-        "product_quantity": 2,
-        "product_price": "2.00",
-        "line_item_total_price": "4.00"
-    },
-    {
-        "id": 1,
-        "product_id": 3,
-        "name": "product_name_03",
-        "product_quantity": 3,
-        "product_price": "3.00",
-        "line_item_total_price": "9.00"
-    },
-    {
-        "id": 1,
-        "product_id": 4,
-        "name": "product_name_04",
-        "product_quantity": 4,
-        "product_price": "4.00",
-        "line_item_total_price": "16.00"
-    }
-];
 
 describe('<Dashboard /> component tests', () => {
     beforeEach(() => render(<Dashboard />));
@@ -121,11 +43,14 @@ describe('<Dashboard /> component tests', () => {
 describe('<Dashboard /> feature tests', () => {
     beforeEach(() => render(<Dashboard />));
 
+    // FIXME: these tests don't look right.
     it('displays order info', () => {
-        expect(true).toBeFalsy();
+        const theVal = screen.getAllByRole('presentation', {name: /order/i});
+        expect(theVal.length).toBe(2);
     })
     it('displays cart info', () => {
-        expect(true).toBeFalsy();
+        const theVal = screen.getAllByRole('presentation', {name: /cart/i});
+        expect(theVal.length).toBe(5);
     })
     
     it('creates a snapshot', () => {
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,20 +2,103 @@ import NavBar from '../nav/NavBar';
 import BrowseSearch from '../nav/BrowseSearch';
 
 export default function Dashboard () {
+    const mockOrderData = [{
+        "order": {
+            "cart": {
+                "cart_id": 1,
+                "cart_items": [
+                    {
+                        "product_id": 1,
+                        "product_name": "product_name_01",
+                        "product_price": 1,
+                        "product_quantity": 1,
+                        "product_total_price": 1
+                    },
+                    {
+                        "product_id": 2,
+                        "product_name": "product_name_02",
+                        "product_price": 2,
+                        "product_quantity": 2,
+                        "product_total_price": 4
+                    },
+                    {
+                        "product_id": 3,
+                        "product_name": "product_name_03",
+                        "product_price": 3,
+                        "product_quantity": 3,
+                        "product_total_price": 9
+                    },
+                    {
+                        "product_id": 4,
+                        "product_name": "product_name_04",
+                        "product_price": 4,
+                        "product_quantity": 4,
+                        "product_total_price": 16
+                    }
+                ]
+            },
+            "user_id": 1,
+            "order_id": 1,
+            "order_date": null,
+            "total_price": {
+                "sum": 30,
+                "cart_id": 1
+            }
+        }
+    }];
+
+    const mockCartData = [
+        {
+            "id": 1,
+            "product_id": 1,
+            "name": "product_name_01",
+            "product_quantity": 1,
+            "product_price": "1.00",
+            "line_item_total_price": "1.00"
+        },
+        {
+            "id": 1,
+            "product_id": 2,
+            "name": "product_name_02",
+            "product_quantity": 2,
+            "product_price": "2.00",
+            "line_item_total_price": "4.00"
+        },
+        {
+            "id": 1,
+            "product_id": 3,
+            "name": "product_name_03",
+            "product_quantity": 3,
+            "product_price": "3.00",
+            "line_item_total_price": "9.00"
+        },
+        {
+            "id": 1,
+            "product_id": 4,
+            "name": "product_name_04",
+            "product_quantity": 4,
+            "product_price": "4.00",
+            "line_item_total_price": "16.00"
+        }
+    ];
+
     return (
         <div>
             <NavBar calledFrom="dashboard" />
             <BrowseSearch />
 
+            {/* FIXME: show actual data from mocks... */}
             <div role="presentation" aria-label="orders">
-                <div>Order 01</div>
-                <div>Order 02</div>
-                <div>Order 03</div>
+                {mockOrderData.map((item, index) => (
+                    // <div role="presentation" aria-label="order">order_id: {item.order_id} user_id: {item.user_id} order_date: {item.order_date} total_price: {item.total_price.sum}</div>
+                    <div role="presentation" aria-label='order' key={index}>order</div>
+                ))}     
             </div>
             <div role="presentation" aria-label="carts">
-                <div>Cart 01</div>
-                <div>Cart 02</div>
-                <div>Cart 03</div>
+                {mockCartData.map((item, index) => (
+                    // <div role="presentation" aria-label="order">order_id: {item.order_id} user_id: {item.user_id} order_date: {item.order_date} total_price: {item.total_price.sum}</div>
+                    <div role="presentation" aria-label='cart' key={index}>cart</div>
+                ))}  
             </div>
         </div>
     )
