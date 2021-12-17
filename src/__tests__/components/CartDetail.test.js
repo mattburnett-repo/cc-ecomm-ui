22b3b07197d79f98e79ca2055d30de7a7391907d
Matt Burnett <mburnett@tutanota.de>
2021-12-17 15:22:21 +0100

    NavBar components / tests started

--- src/__tests__/components/CartDetail.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Cart from '../../components/cart/Cart';
-
-describe('Cart', () => {
-    test('it has a cart detail component', () => {
-
-    })
-})
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,8 @@
+export default function NavBar () {
+    return (
+        <div>
+            <div role="presentation" aria-label="header-message">header message</div>
+            <button aria-label="logout">Log Out</button>
+        </div>
+    )
+}
