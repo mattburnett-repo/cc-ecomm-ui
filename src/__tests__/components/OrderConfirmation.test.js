c90c7aaa42062ff0d95df4bf317b703dc1e8b162
Matt Burnett <mburnett@tutanota.de>
2021-12-17 14:52:26 +0100

    OrderConfirmation component / test

--- src/__tests__/components/OrderConfirmation.test.js
+++ src/__tests__/components/OrderConfirmation.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import OrderConfirmation from '../../components/order/Order';
+// 20211217: Wireframe https://wireframe.cc/KF5blD
 
-describe('OrderDonfirmation', () => {
-    test('it has an order confirmation component', () => {
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
 
+import OrderConfirmation from '../../components/order/OrderConfirmation';
+
+describe('<OrderConfirmation /> component tests', () => {
+    beforeEach(() => render(<OrderConfirmation />));
+
+    it('should display confirmation info', () => {
+        screen.getByRole('presentation', {name: /confirmation-info/i});
     });
-})
+    it('should display a home button', () => {
+        screen.getByRole('button', {name: /home/i});
+    });
+    it('should display a logout button', () => {
+        screen.getByRole('button', {name: /logout/i});
+    });
+
+}); // end component
+
+describe('<OrderConfirmation /> feature tests', () => {
+    beforeEach(() => render(<OrderConfirmation />));
+
+    it('creates a snapshot', () => {
+        const tree = renderer.create(<OrderConfirmation />).toJSON();
+        expect(tree).toMatchSnapshot();  
+    });
+}); // end feature
+
+describe('<OrderConfirmation /> unit / functional tests', () => {
+    beforeEach(() => render(<OrderConfirmation />));
+
+    it('clicks the home button', () => {
+        const theVal = screen.getByRole('button', {name: /home/i});
+        fireEvent.click(theVal);
+    });
+    it('clicks the logout button', () => {
+        const theVal = screen.getByRole('button', {name: /logout/i});
+        fireEvent.click(theVal);
+    });
+}); // end unit / functional
---
+++ src/components/order/OrderConfirmation.js
@@ -0,0 +1,15 @@
+export default function OrderConfirmation () {
+    return (
+        <div>
+            <div role="presentation" aria-label="confirmation-info">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
+                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            </div>
+            <button aria-label="home">Home</button>
+            <button aria-label="logout">Log Out</button>
+        </div>
+    )
+}
