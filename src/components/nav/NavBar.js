ee6297aafd111b6ad645c5a527dbabd99c73b248
Matt Burnett <mburnett@tutanota.de>
2021-12-17 17:49:42 +0100

    refactored NavBar, updated tests

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,7 +1,21 @@
-export default function NavBar () {
+export default function NavBar (props) {
+    const { calledFrom } = props;
+
     return (
         <div>
             <div role="presentation" aria-label="header-message">header message</div>
+            if(calledFrom !== 'landing') {
+                <>
+                    <button aria-label="home">Home</button>
+                    <button aria-label="show-cart">Show Cart</button>
+                </>
+            };
+            if(calledFrom === 'cart') {
+                <button aria-label="checkout">Checkout</button>
+            }
+            if(calledFrom === 'shippingInfo' || calledFrom === 'paymentInfo') {
+                <button aria-label="cancel">Cancel</button>
+            }
             <button aria-label="logout">Log Out</button>
         </div>
     )
