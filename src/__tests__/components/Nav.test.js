745c1920e3a11eb884681b76f96cc835540d319b
Matt Burnett <mburnett@tutanota.de>
2021-12-17 13:49:54 +0100

    added Nav test. doesn't work yet

---
+++ src/__tests__/components/Nav.test.js
@@ -0,0 +1,9 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import userEvent from '@testing-library/user-event';
+
+describe('<Nav /> component tests', () => {
+    it('doesnt work', () => {
+        expect(true).toBeFalsy();
+    })
+})
