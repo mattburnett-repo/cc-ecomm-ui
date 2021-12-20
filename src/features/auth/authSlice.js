549bbb6c43a55ed1aaffa3be63fbe8c66d886cb3
Matt Burnett <mburnett@tutanota.de>
2021-12-20 21:15:53 +0100

    stubbed out api call, uncommed test code, while we figure out what to do next

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,14 +1,64 @@
 import { createSlice, createAsyncThunk }  from '@reduxjs/toolkit';
+import userEvent from '@testing-library/user-event';
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 console.log('api base url ' + API_BASE_URL);
 
-export const localAuth = createAsyncThunk (
-    'auth/localAuth',
+export const localAuthLogin = createAsyncThunk (
+    'auth/localAuthLogin',
     async ( props ) => {
         const { username, password } = props;
         console.log('authSlice: username: ' + username + ' password: ' + password);
 
+        // TODO: API call goes here... Use Passport local strategy.
+        //      this fails, and sends back Swagger doc
+        //          we want to keep the local / oauth login strategies on the api server
+        //              but we need to be able to log in here as well
+        //          maybe have separate login routes on api server for remote login
+        //              zB /auth/login, auth/oauth
+        //          OR
+        //              spend a lot of time trying to make something that can't work, work...
+        //      look for token / localstorage???
+        
+        // try {
+        //     const response = await fetch(
+        //         API_BASE_URL,
+        //         {
+        //         method: "POST",
+        //         headers: {
+        //             Accept: "application/json, application/x-www-form-urlencoded",
+        //             // "Content-Type": "application/json",
+        //             "Content-Type": "application/x-www-form-urlencoded",
+        //         },
+        //         body: JSON.stringify({
+        //             username: username,
+        //             password: password,
+        //         }),
+        //         }
+        //     )
+
+        //     // let data = await response.json()
+        //     let data = await response;
+
+        //     // console.log("data ", data)
+
+        //     if (response.status === 200) {
+        //     //   localStorage.setItem("token", data.token)
+        //     //   return { ...data, username: name, email: email }
+        //         let userData = {user_id: 4, username: username, someFakeAuthToken: 'asdlfue84to53fkasjhgkah'} // FIXME: better as an array?
+
+        //          return {userData: userData, isAuthorized: true}  
+        //     } else {
+        //     //   return thunkAPI.rejectWithValue(data)
+        //        console.log('authSlice un successful')
+        //        return {message: 'login un successful', isAuthorized: false} 
+        //     }
+        // } catch (e) {
+        // console.log("Error", e.response.data)
+        // // return thunkAPI.rejectWithValue(e.response.data)
+        // }
+
+        // basic test
         try {
             if(username === 'asdf' && password === 'asdf') {
                 console.log('authSlice successful')
@@ -17,20 +67,19 @@ export const localAuth = createAsyncThunk (
 
                 return {userData: userData, isAuthorized: true}        // TODO: not sure if this is... legal, but it works...
             } else {
-                console.log('authSlice un successful')
+                // console.log('authSlice un successful')
                 return {message: 'login un successful', isAuthorized: false}    // TODO: not sure if this is... legal, but it works...
-
-            }
+            }          
         } catch (e) {   
-            console.log('error in auth/localAuth: ' + e);
-        } // end try catch
+            console.log('error in auth/localAuthLogin: ' + e);
+        } // end basic test
     }
-) // end localAuth
+) // end localAuthLogin
 
 const options = {
     name: 'auth',
     initialState: {
-        userData: [{message: 'nope'}],
+        userData: '',
         isLoading: false,
         hasError: false,
         errorMsg: '',
@@ -43,20 +92,21 @@ const options = {
     },
     extraReducers: (builder) => {
         builder
-            .addCase(localAuth.pending, (state) => {
+            .addCase(localAuthLogin.pending, (state) => {
                 state.isLoading = true;
                 state.hasError = false;
                 state.isAuthorized = false;
                 state.errorMsg = '';
             })
-            .addCase(localAuth.fulfilled, (state, action) => {
+            .addCase(localAuthLogin.fulfilled, (state, action) => {
                 state.userData = action.payload.userData;
                 state.isLoading = false;
                 state.hasError = false;
                 state.isAuthorized = action.payload.isAuthorized;
+                // state.isAuthorized = true
                 state.errorMsg = '';
             })
-            .addCase(localAuth.rejected, (state, action) => {
+            .addCase(localAuthLogin.rejected, (state, action) => {
                 state.isLoading = false;
                 state.hasError = true;
                 state.errorMsg = action.error; 
--- src/features/auth/login.js
+++ src/features/auth/login.js
@@ -1,28 +1,33 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { localAuth, selectIsAuthorized } from './authSlice';
+import { localAuthLogin, selectIsAuthorized } from './authSlice';
 
 import LoginDisplay from "../../components/auth/LoginDisplay";
 
 export default function Login () {
-    // get data from API / Redux?
+    // get data from API / Redux
     //      just get test output in console from reducer, for now
     const isAuthorized = useSelector(selectIsAuthorized);
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-        // dispatch(authLocalLoginTestOutput())
-        dispatch(localAuth({username: 'asdf', password: 'asdf'})); // this should pass / fulfill / isAuthorized: true
-        // dispatch(localAuth({username: 'qwer', password: 'qwer'})); // this should fail / reject / isAuthorized: false
+        // dispatch should be called from form submit / oauth button click, not on load, here
+        // dispatch(localAuthLogin({username: 'asdf', password: 'asdf'})); // this should pass / fulfill / isAuthorized: true
+        dispatch(localAuthLogin({username: 'qwer', password: 'qwer'})); // this should fail / reject / isAuthorized: false
     }, [dispatch]);
     
     // xx-auth and login button should dispatch actions to redux
     
     // create onClick function/s for google / github / local logins
     if(isAuthorized) {
-        return <h1>login.js sees authorization</h1>
+        return (
+            <div>
+                <h1>login.js sees authorization</h1>
+                <h2>should redirect to dashboard</h2>            
+            </div>
+        )
     } else {
         return <LoginDisplay />
     }
