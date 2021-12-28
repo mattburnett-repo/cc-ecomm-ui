fa556c8bfdd63743456d36b94e2939da50be5dd6
Matt Burnett <mburnett@tutanota.de>
2021-12-28 01:21:09 +0100

    product detail now gets a random id number. kind of like a featured product thing

--- src/components/dashboard/DashboardDisplay.js
+++ src/components/dashboard/DashboardDisplay.js
@@ -6,11 +6,13 @@ import ProductsListing from '../../features/products/productsListing'
 import ProductDetail from '../../features/product/productDetail'
 
 export default function DashboardDisplay () {
+    const randomProductId =  Math.floor(Math.random() * 31);
+
     return (
         <div>
             <NavBar calledFrom="dashboard" />
             <BrowseSearch />
-            <ProductDetail productId="31"/> 
+            <ProductDetail productId={randomProductId}/> 
             <Orders />
             <Carts />
             <ProductsListing />
