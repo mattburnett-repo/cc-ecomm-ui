8e958f12a1f0fb77c06f17e13c32b7e715a55aae
Matt Burnett <mburnett@tutanota.de>
2021-12-29 20:11:36 +0100

    added 'no carts / orders to show' messages when there is no data for cart / order

--- src/components/carts/CartsDisplay.js
+++ src/components/carts/CartsDisplay.js
@@ -9,16 +9,18 @@ export default function CartsDisplay ( props )  { // TODO: cartData should come 
         <div role="presentation" aria-label="carts">
             <h2>CartsDisplay</h2>
             <h4>Saved Carts</h4>
-            {savedCarts.map((item, index) => (
-                <div role="presentation" aria-label="cart-item" key={index}>
-                    <form onSubmit={handleCartClick} method="post">
-                        <input type="hidden" name="cart_id" value={item.cart.cart_id} />
-                        cart_id: {item.cart.cart_id}  
-                        user_id: {item.cart.user_id}
-                        <input type="submit" aria-label="go-to-cart" />
-                    </form>
-                </div>
-            ))}
+            {(savedCarts.length > 0) ? (
+                savedCarts.map((item, index) => (
+                    <div role="presentation" aria-label="cart-item" key={index}>
+                        <form onSubmit={handleCartClick} method="post">
+                            <input type="hidden" name="cart_id" value={item.cart.cart_id} />
+                            cart_id: {item.cart.cart_id}  
+                            user_id: {item.cart.user_id}
+                            <input type="submit" aria-label="go-to-cart" />
+                        </form>
+                    </div>
+                )
+            )) : (<h5>No saved carts to show</h5>)}
          </div>
     )    
 }
--- src/components/orders/OrdersDisplay.js
+++ src/components/orders/OrdersDisplay.js
@@ -7,18 +7,20 @@ export default function OrdersDisplay ( props ) {
     return (
         <div role="presentation" aria-label="orders">
             <h2>OrdersDisplay</h2>
-            {ordersData.map((item, index) => (
-                <div role="presentation" aria-label="order-item" key={index}>
-                    <form onSubmit={handleOrderClick} method="post">
-                        <input type="hidden" name="order_id" value={item.order.order_id} />
-                        order_id: {item.order.order_id} 
-                        user_id: {item.order.user_id} 
-                        order_date: {item.order.order_date} 
-                        total_price: {item.order.total_price.sum}
-                        <input type="submit" aria-label="go-to-order" /> 
-                      </form>
-                </div>
-            ))}     
+            {(ordersData.length > 0 ) ? (
+                ordersData.map((item, index) => (
+                    <div role="presentation" aria-label="order-item" key={index}>
+                        <form onSubmit={handleOrderClick} method="post">
+                            <input type="hidden" name="order_id" value={item.order.order_id} />
+                            order_id: {item.order.order_id} 
+                            user_id: {item.order.user_id} 
+                            order_date: {item.order.order_date} 
+                            total_price: {item.order.total_price.sum}
+                            <input type="submit" aria-label="go-to-order" /> 
+                        </form>
+                    </div>
+                )
+            )) : (<h5>No orders to show</h5>)}     
         </div>
     )
 }
