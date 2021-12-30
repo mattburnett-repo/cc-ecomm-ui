1c7ec2dbce7cbefd148c1f44c8bc201e5c2cce56
Matt Burnett <mburnett@tutanota.de>
2021-12-30 17:45:33 +0100

    cleanup after productDetail-dev

--- src/App.js
+++ src/App.js
@@ -12,27 +12,20 @@ import CartDetail from './features/cart/cartDetail'
 //        import 'bootstrap/dist/css/bootstrap.min.css'
 //        reactstrap: https://youtu.be/R54neaLznFA?t=923
 // TODO: React transition npm i react-transition-group
-//        https://youtu.be/fcna-jJtAXk?t=116
+//          https://youtu.be/fcna-jJtAXk?t=116
+
+//  path='...' exact component={ () => <Component someProp= {propVal}} />
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-          {/* <Route path='/' exact><Login /></Route>
-          <Route path='/login' exact><Login /></Route>
-          <Route path='/register' exact><Register /></Route>
-          <Route path='/dashboard' exact><Dashboard /> </Route>
-          <Route path="/product-detail/:id" exact><ProductDetail /></Route> 
-          <Route path='/cart-detail' exact><CartDetail /> </Route> */}
-
-          {/* ... component={ () => <Component someProp= {propVal}} /> */}
           <Route path='/' exact component={Login} />
           <Route path='/login' exact component={Login} />
           <Route path='/register' exact component={Register} />
           <Route path='/dashboard' exact component={Dashboard} />
           <Route path="/product-detail/:id" exact component={ProductDetail} />
-          {/* <Route path="/product-detail/:id" exact component={ () => <ProductDetail productId={id} />} /> */}
           <Route path='/cart-detail' exact component={CartDetail} />
         </Switch>
       </div>
