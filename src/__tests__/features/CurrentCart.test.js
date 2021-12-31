fdcac0b6aaebae03f308f998ee0eb3566b1f38fd
Matt Burnett <mburnett@tutanota.de>
2021-12-31 19:10:51 +0100

    pospone developing CurrenCart tests for now, since we're behind schedule

--- src/__tests__/features/CurrentCart.test.js
+++ src/__tests__/features/CurrentCart.test.js
@@ -1,13 +1,82 @@
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import renderer from 'react-test-renderer';
+
+// eslint-disable-next-line
+import { render, screen, fireEvent } from '@testing-library/react';
+// eslint-disable-next-line
+import renderer from 'react-test-renderer';
 // // import userEvent from '@testing-library/user-event';
 
 // import { Provider } from 'react-redux';
 // import store from '../../store'
 
+// eslint-disable-next-line
+import cartsSlice, { addItemToCurrentCart } from '../../features/carts/cartsSlice';
+
+// MOCK DATA
+
+// eslint-disable-next-line
+const mockItemOne = [{
+    "id": 1,
+    "category_id": 1,
+    "name": "hedgehog",
+    "description": "product_desc_01",
+    "image_url": "https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_1280.jpg",
+    "price": "1.00"
+}]
+
+// eslint-disable-next-line
+const mockItemTwo ={
+    "id": 2,
+    "category_id": 1,
+    "name": "maine coon cat",
+    "description": "prod desc 02",
+    "image_url": "https://cdn.pixabay.com/photo/2015/03/27/13/16/maine-coon-694730_1280.jpg",
+    "price": "1.00"
+}
+
+// eslint-disable-next-line
+const mockItemThree = {
+    "id": 3,
+    "category_id": 1,
+    "name": "bulldog",
+    "description": "prod desc 03",
+    "image_url": "https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518_1280.jpg",
+    "price": "1.00"
+}
+
+// *** It is better to figure out how to make these tests work, but time is short and we need
+//      to keep moviing, so for now we just leave in the test.todo() entries
+
+// https://medium.com/@netxm/testing-redux-reducers-with-jest-6653abbfe3e1
+//      "It’s important to test reducers. That’s where business logic should happen and where 
+//       new application state is formed based on external (API) or internal response."
+// https://redux.js.org/usage/writing-tests#reducers
+//      "In the majority of cases, the reducer is an implementation detail that does not need explicit tests."
+
+// START TESTS
+
 describe('CurrentCart initial feature test', () => {
-    test.todo('it should exist in the store')
+    test.todo('should return the initial state')
+    // test('should return the initial state', () => {
+    //     expect(cartsSlice(undefined, {})).toEqual(
+    //         {"currentCart": [], "errorMsg": "", "hasError": false, "isLoading": false, "savedCarts": []}
+    //     )
+    //   })
+
     test.todo('it should add an item')
+    // test('it should add an item', () => {
+    //     const previousState = []
+    //     expect(cartsSlice(previousState, addItemToCurrentCart({mockItemOne}))).toEqual([
+    //         {
+    //             "id": 1,
+    //             "category_id": 1,
+    //             "name": "hedgehog",
+    //             "description": "product_desc_01",
+    //             "image_url": "https://cdn.pixabay.com/photo/2014/10/01/10/44/hedgehog-468228_1280.jpg",
+    //             "price": "1.00"
+    //         }
+    //     ])
+    //   })
+
     test.todo('if an item is already in the cart, the item quantity should increase when adding the item again')
     test.todo('it should increase an items quantity')
     test.todo('it should decrease an items quantity')
@@ -15,11 +84,10 @@ describe('CurrentCart initial feature test', () => {
     test.todo('it should save the cart to database')
     test.todo('it should reinitialize')
 
-    test.todo('it should render a snapshot')
+    // test.todo('it should render a snapshot')
 }) // end CurrentCart feature test
 
 describe('CurrentCart saved cart feature test', () => {
-    // saved carts
     test.todo('it should load a saved cart as current cart')
     test.todo('it should add an item to a saved cart')
     test.todo('if an item is already in the saved cart, the item quantity should increase')
@@ -28,5 +96,5 @@ describe('CurrentCart saved cart feature test', () => {
     test.todo('it should remove a saved carts item')
     test.todo('it should save an updated saved cart to the database')
 
-    test.todo('it should render a snapshot')
+    // test.todo('it should render a snapshot')
 }) // end CurrentCart saved cart feature test
