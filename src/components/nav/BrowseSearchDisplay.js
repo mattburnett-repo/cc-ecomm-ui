278b6e13c27db0308a3676cabf819a585dad57be
Matt Burnett <mburnett@tutanota.de>
2022-01-15 11:36:36 +0100

    browseSearch

--- src/components/nav/BrowseSearchDisplay.js
+++ src/components/nav/BrowseSearchDisplay.js
@@ -7,9 +7,9 @@ export default function BrowseSearchDisplay (props) {
 
     return (
         <StyledBrowseSearch>
-            {/* <div role="presentation" aria-label="browse-search"> */}
+            <div className='browse-search-container' role="presentation" aria-label="browse-search">
                 <div role="presentation" aria-label="browse">
-                    <label htmlFor="product-categories">Product Categories: </label>
+                    <label className='browse' htmlFor="product-categories">Product Categories: </label>
                     <select role="presentation" id="product-categories" name="productCategories" aria-label="product-categories" defaultValue="0" onChange={handleChange}>
                         <option key="0" value="0">Show all products</option>
                         {data.map(item => (
@@ -17,7 +17,7 @@ export default function BrowseSearchDisplay (props) {
                         ))}                    
                     </select>
                 </div>
-                <div role="presentation" aria-label="search">
+                <div className="search" role="presentation" aria-label="search">
                     <form onSubmit={handleSearch} method="post">
                         <div>
                             <label htmlFor="searchTerms">Search: </label>
@@ -28,7 +28,7 @@ export default function BrowseSearchDisplay (props) {
                         </div>
                     </form>
                 </div>
-            {/* </div> */}
+            </div>
         </StyledBrowseSearch>
     )           
 }
--- src/components/styles/BrowseSearch.styled.js
+++ src/components/styles/BrowseSearch.styled.js
@@ -2,22 +2,42 @@
 import styled from 'styled-components'
 
 export const StyledBrowseSearch = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: top;
-    justify-content: space-between;
+    .browse-search-container {
+        display: flex;
+        flex-direction: column;
+        align-items: top;
+        justify-content: space-between;
 
-    max-width: 100%;
-    background-color: ${({ theme }) => theme.colors.light};
-    margin: 20px auto;
-    padding: 40px 40px;
+        max-width: 100%;
+        background-color: ${({ theme }) => theme.colors.light};
+        margin: 20px auto;
+        padding: 40px 40px;
 
-    font-size: 1.5rem;
-    text-align: center;
-    color: ${({ theme }) => theme.colors.secondary};
-    
-    border: 1px solid black;
-    border-radius: 10px;
+        font-size: 1.5rem;
+        text-align: center;
+        color: ${({ theme }) => theme.colors.secondary};
+        
+        border: 1px solid black;
+        border-radius: 10px;        
+    }
+
+    @media (min-width: 70rem) {
+        .browse-search-container {
+            flex-direction: row;
+        }
+
+        // .product-detail-container > * {
+        //     flex-basis: 100%;
+        // }
+
+    }
+
+    @media(max-width: 70rem) {
+        .browse, .search {
+            font-size: 1rem !important;
+        }
+    }
+
 
     input {
         border: 1px solid ${({ theme }) => theme.colors.info};;
