53c0e19d4689f505a38d948a7d0f58384abc0c21
Matt Burnett <mburnett@tutanota.de>
2022-01-15 11:19:30 +0100

    more nav bar

--- src/components/styles/NavBar.style.js
+++ src/components/styles/NavBar.style.js
@@ -7,12 +7,10 @@ export const StyledNavBar = styled.div`
         grid-template-columns: 1fr 2fr 1fr;
         justify-items: center;
 
-        // max-width: 100%;
         background-color: ${({ theme }) => theme.colors.light};
-        // margin: 10px 0 auto;
+
         padding: 40px 40px;
 
-        // text-align: center;
         color: ${({ theme }) => theme.colors.secondary};
 
         border: 1px solid black;
@@ -24,14 +22,6 @@ export const StyledNavBar = styled.div`
             grid-template-columns: 1fr;
             font-size: 1rem;
         }
-
-        // .nav-bar-container > * {
-        //     flex-basis: 100%;
-        // }
-
-        // .nav-bar-container > * + * {
-        //     margin-left: 2em;
-        // }
 
         button {
             margin-top: 2rem;
